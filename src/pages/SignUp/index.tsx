ba24c0b23a439daa96de7b4bc27e60a08b525687
arthurgehrke <arthurgehrke07@gmail.com>
2020-07-15 10:36:37 -0300

    chore: add a function into utils component that will get all input errors and display them inside of the input

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logo from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -11,8 +14,12 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('The name is required'),
         email: Yup.string()
@@ -28,7 +35,9 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     } catch (err) {
-      console.log(err);
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -37,7 +46,7 @@ const SignUp: React.FC = () => {
       <Background />
       <Content>
         <img src={logo} alt="BooksLogo" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Realize seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="nome" />
