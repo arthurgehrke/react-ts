e681a85bdf87812fc31c84f22337e6f43939c648
arthurgehrke <arthurgehrke07@gmail.com>
2020-07-15 08:56:15 -0300

    chore: start form validation using yup into signup page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import logo from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -10,9 +11,26 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('The name is required'),
+        email: Yup.string()
+          .required('The email is required')
+          .email('This email is invalid'),
+        password: Yup.string().min(
+          6,
+          'The password requires at least 6 characters',
+        ),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
